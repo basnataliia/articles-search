4f089c7608f5f21dc5d0e22b1acf228dbab081d0
Brad-PC\Natalie <natalie.basmanova@futurevaultinc.com>
2017-01-30 22:38:32 -0500

    added tests

--- src/components/search-article.test.js
+++ src/components/search-article.test.js
@@ -4,19 +4,42 @@ import { shallow } from 'enzyme';
 import TestUtils from 'react-addons-test-utils';
 import SearchArticle from './search-article';
 import sinon from 'sinon';
+import RaisedButton from 'material-ui/RaisedButton';
+import TextField from 'material-ui/TextField';
 
 describe('<SearchArticle />', () => {
 
   it('Should fire searchForArticle when that button is clicked', () => {
 
-    const wrapper = shallow(<SearchArticle />);
-    const spy = sinon.spy(wrapper.instance(), "searchForArticle");
-	wrapper.setState({articleQuery: 'Madonna'});
-	wrapper.update();
-	wrapper.find('button').simulate('click');
-	
-    expect(spy.calledOnce).to.equal(true);
+    const callback = sinon.spy();
+    const wrapper = shallow(
+      <SearchArticle searchForArticle={() => {}}/>
+    );
+
+    wrapper.find(RaisedButton).simulate('click');
+    expect(callback).to.have.been.calledOnce;
 
   });
+
+	  it('should render an <RaisedButton />', () => {
+
+	    const wrapper = shallow(
+	      <SearchArticle searchForArticle={() => {}}/>
+	    );
+
+	    expect(wrapper.find(RaisedButton)).to.have.length(1);
+
+	});
+
+
+	   it('should render an <TextField />', () => {
+
+	    const wrapper = shallow(
+	      <SearchArticle searchForArticle={() => {}}/>
+	    );
+
+	    expect(wrapper.find(TextField)).to.have.length(1);
+
+	});
 
 });
